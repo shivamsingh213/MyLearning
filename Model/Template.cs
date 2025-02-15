04267157114df87584c9890ae5a687d3247f439d
shivamsingh213 <89403023+shivamsingh213@users.noreply.github.com>
2025-02-15 18:23:59 +0530

    Add files via upload

---
+++ Model/Template.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XDocument.Model
+{
+    public class Template
+    {
+        public List<Configuration> Configurations { get; set; }
+
+    }
+    public class Configuration
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Description: {Description}";
+        }
+
+    }
+}
+
+
+
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Globalization;
+using System.Reflection;
+using System.Text.Json;
+using XDocument.Model;
+
+
+class Program
+{
+  
+    public class GetSerilizerClass
+    {
+        private readonly string _filepath;
+        public GetSerilizerClass(string filepath)
+        {
+            this._filepath = filepath;
+        }
+
+        public Template GetJsonFile()
+        {
+            if (!File.Exists(_filepath))
+            {
+                Console.WriteLine("JSON file not found.");
+                return new Template { Configurations = new List<Configuration>() };
+            }
+            using FileStream json = File.OpenRead(_filepath);
+               Template teachers = JsonSerializer.Deserialize<Template>(json) ?? new Template { Configurations = new List<Configuration>() };
+            return teachers;
+
+        }
+    }
+    public static void Main()
+    {
+       string path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        Console.WriteLine(path);
+        string jsonpathname = Path.Combine(path, "Template.json");
+        Console.WriteLine(jsonpathname);
+        try
+        {
+            GetSerilizerClass serializer = new GetSerilizerClass(jsonpathname);
+            Template teachers = serializer.GetJsonFile();
+            foreach (Configuration config in teachers.Configurations)
+            {
+                 Console.WriteLine(config.ToString());
+            }
+            Console.ReadLine();
+        }
+        catch (Exception ex) { 
+        Console.WriteLine(ex.ToString());
+            Console.ReadLine();
+        }
+    }
+}
